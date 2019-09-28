7ed09af880d3b8e21ea8068c58380c62f9bfe793
lagercat <malessebastian@gmail.com>
2019-09-28 03:10:03 +0200

    Track KM Script

---
+++ get_track_km.py
@@ -0,0 +1,25 @@
+import os
+import PIL.ExifTags
+import PIL.Image as pil
+
+directory = os.fsencode('.')
+
+for file in os.listdir(directory):
+    image_name = os.fsdecode(file)
+    if image_name.endswith(".JPG"):
+        img = pil.open(image_name)
+        exif = {
+            PIL.ExifTags.TAGS[k]: v
+            for k, v in img._getexif().items()
+            if k in PIL.ExifTags.TAGS
+        }
+        gps_info = exif['GPSInfo']
+        string_longitude = gps_info[2]
+        string_latitude = gps_info[4]
+        latitude = string_latitude[0][0] / string_latitude[0][1] + \
+            (string_latitude[1][0] / string_latitude[1][1] +
+                (string_latitude[2][0] / string_latitude[2][1]) / 60) / 60
+        longitude = string_longitude[0][0] / string_longitude[0][1] + \
+            (string_longitude[1][0] / string_longitude[1][1] +
+                (string_longitude[2][0] / string_longitude[2][1]) / 60) / 60
+        print(latitude, longitude)
