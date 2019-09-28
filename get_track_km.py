19e8fbcff0ec76edd7663a5aaa9193dbd211772f
lagercat <malessebastian@gmail.com>
2019-09-28 05:09:10 +0200

    Subdirs

--- get_track_km.py
+++ get_track_km.py
@@ -9,42 +9,44 @@ root_dir = '/siemens/data/Trackpictures/Trackpictures_HiRes/'
 count = 1
 
 for _, dirs, _, in os.walk(root_dir):
-    filelist = glob.glob(os.path.join(root_dir, '*'))
-    directory_string = '/siemens/data/Trackpictures/Trackpictures_HiRes/' + dirs
-    print(directory_string)
-    directory = os.fsencode(directory_string)
+    for sub_dir in dirs:
+        filelist = glob.glob(os.path.join(root_dir, '*'))
+        directory_string = '/siemens/data/Trackpictures/Trackpictures_HiRes/' + \
+                            sub_dir
+        print(directory_string)
+        directory = os.fsencode(directory_string)
 
-    past_coordinates = (-1, -1)
+        past_coordinates = (-1, -1)
 
-    total_distance = 0
-    results = {}
-    for infile in sorted(filelist):
-        image_name = str(infile)
-        if image_name.endswith(".JPG"):
-            img = pil.open(image_name)
-            exif = {
-	      PIL.ExifTags.TAGS[k]: v
-              for k, v in img._getexif().items()
-              if k in PIL.ExifTags.TAGS
-            }
-            gps_info = exif['GPSInfo']
-            string_latitude = gps_info[2]
-            string_longitude = gps_info[4]
-            latitude = string_latitude[0][0] / string_latitude[0][1] + \
-                (string_latitude[1][0] / string_latitude[1][1] +
-                    (string_latitude[2][0] / string_latitude[2][1]) / 60) / 60
-            longitude = string_longitude[0][0] / string_longitude[0][1] + \
-                (string_longitude[1][0] / string_longitude[1][1] +
-                    (string_longitude[2][0] / string_longitude[2][1]) / 60) / 60
+        total_distance = 0
+        results = {}
+        for infile in sorted(filelist):
+            image_name = str(infile)
+            if image_name.endswith(".JPG"):
+                img = pil.open(image_name)
+                exif = {
+                  PIL.ExifTags.TAGS[k]: v
+                  for k, v in img._getexif().items()
+                  if k in PIL.ExifTags.TAGS
+                }
+                gps_info = exif['GPSInfo']
+                string_latitude = gps_info[2]
+                string_longitude = gps_info[4]
+                latitude = string_latitude[0][0] / string_latitude[0][1] + \
+                    (string_latitude[1][0] / string_latitude[1][1] +
+                        (string_latitude[2][0] / string_latitude[2][1]) / 60) / 60
+                longitude = string_longitude[0][0] / string_longitude[0][1] + \
+                    (string_longitude[1][0] / string_longitude[1][1] +
+                        (string_longitude[2][0] / string_longitude[2][1]) / 60) / 60
 
-            if past_coordinates[0] >= 0:
-                distance = geopy.distance.distance((latitude, longitude),
-                                                   past_coordinates).km
-                results[image_name] = distance + total_distance
-                total_distance += distance
-            else:
-                results[image_name] = 0
-            past_coordinates = (latitude, longitude)
+                if past_coordinates[0] >= 0:
+                    distance = geopy.distance.distance((latitude, longitude),
+                                                       past_coordinates).km
+                    results[image_name] = distance + total_distance
+                    total_distance += distance
+                else:
+                    results[image_name] = 0
+                past_coordinates = (latitude, longitude)
 
-with open('track_km.json', 'w+') as fp:
-    json.dump(results, fp)
+        with open('track_km +' + count + '.json', 'w+') as fp:
+            json.dump(results, fp)
