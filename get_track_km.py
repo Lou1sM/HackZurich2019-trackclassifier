9972fe6a96e7937ca3bb35bf853471b4452e3d03
lagercat <malessebastian@gmail.com>
2019-09-28 14:05:38 +0200

    Updated get track_km

--- get_track_km.py
+++ get_track_km.py
@@ -6,47 +6,56 @@ import PIL.Image as pil
 import geopy.distance
 
 root_dir = '/siemens/data/Trackpictures/Trackpictures_HiRes/'
-count = 1
+sub_dirs = ['001_-_Landquart_-_Klosters_Platz/',
+            '002_-_Klosters_Platz_-_Landquart/']
 
-for _, dirs, _, in os.walk(root_dir):
-    for sub_dir in dirs:
-        directory_string = '/siemens/data/Trackpictures/Trackpictures_HiRes/' + \
-                            sub_dir
-        past_coordinates = (-1, -1)
+total_distance = 0
+results = {}
 
-        print(directory_string)
-        total_distance = 0
-        results = {}
+for sub_dir in sub_dirs:
+    directory_string = '/siemens/data/Trackpictures/Trackpictures_HiRes/' + \
+                        sub_dir
 
-        filelist = glob.glob(os.path.join(directory_string, '*'))
-        for infile in sorted(filelist):
-            image_name = str(infile)
-            if image_name.endswith(".JPG"):
-                img = pil.open(image_name)
-                exif = {
-                  PIL.ExifTags.TAGS[k]: v
-                  for k, v in img._getexif().items()
-                  if k in PIL.ExifTags.TAGS
-                }
-                gps_info = exif['GPSInfo']
-                string_latitude = gps_info[2]
-                string_longitude = gps_info[4]
-                latitude = string_latitude[0][0] / string_latitude[0][1] + \
-                    (string_latitude[1][0] / string_latitude[1][1] +
-                        (string_latitude[2][0] / string_latitude[2][1]) / 60) / 60
-                longitude = string_longitude[0][0] / string_longitude[0][1] + \
-                    (string_longitude[1][0] / string_longitude[1][1] +
-                        (string_longitude[2][0] / string_longitude[2][1]) / 60) / 60
+    filelist = glob.glob(os.path.join(directory_string, '*'))
+    past_coordinates = (-1, -1)
+    local_total = total_distance
 
-                if past_coordinates[0] >= 0:
-                    distance = geopy.distance.distance((latitude, longitude),
-                                                       past_coordinates).km
+    if sub_dir == '001_-_Landquart_-_Klosters_Platz/':
+        arr = sorted(filelist)
+    else:
+        arr = filelist.reverse()
+
+    for infile in arr:
+        image_name = str(infile)
+        if image_name.endswith(".JPG"):
+            img = pil.open(image_name)
+            exif = {
+              PIL.ExifTags.TAGS[k]: v
+              for k, v in img._getexif().items()
+              if k in PIL.ExifTags.TAGS
+            }
+            gps_info = exif['GPSInfo']
+            string_latitude = gps_info[2]
+            string_longitude = gps_info[4]
+            latitude = string_latitude[0][0] / string_latitude[0][1] + \
+                (string_latitude[1][0] / string_latitude[1][1] +
+                    (string_latitude[2][0] / string_latitude[2][1]) / 60) / 60
+            longitude = string_longitude[0][0] / string_longitude[0][1] + \
+                (string_longitude[1][0] / string_longitude[1][1] +
+                    (string_longitude[2][0] / string_longitude[2][1]) /
+                    60) / 60
+
+            if past_coordinates[0] >= 0:
+                distance = geopy.distance.distance((latitude, longitude),
+                                                   past_coordinates).km
+                if sub_dir == '001_-_Landquart_-_Klosters_Platz/':
                     results[image_name] = distance + total_distance
                     total_distance += distance
                 else:
-                    results[image_name] = 0
-                past_coordinates = (latitude, longitude)
+                    results[image_name] = local_total - distance
+            else:
+                results[image_name] = 0
+            past_coordinates = (latitude, longitude)
 
-        with open('track_km +' + str(count) + '.json', 'w+') as fp:
-            json.dump(results, fp)
-        count = count + 1
+with open('track_km.json', 'w+') as fp:
+    json.dump(results, fp)
