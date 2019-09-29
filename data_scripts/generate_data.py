b56690de944889d8437d9a01cb859c26fdaca975
lagercat <malessebastian@gmail.com>
2019-09-29 06:08:51 +0200

    Integrate with NN files

--- data_scripts/generate_data.py
+++ data_scripts/generate_data.py
@@ -3,6 +3,12 @@ import os
 
 OBJECT_CLASSES = ['balise', 'distant_signal', 'main_signal']
 
+
+def clean_name(x):
+    x['name'] = './' + x['name']
+    return x
+
+
 with open('COCO.json') as f:
     base_data = json.load(f)
     images = base_data['images']
@@ -34,6 +40,18 @@ with open('COCO.json') as f:
                     'objects': [0, 0, 0, 0]
                 }
                 result.append(obj)
-    final_result = sorted(result, key=lambda k: k['name'])
+
+    final_result = result
+    ANNOTED_FILES = ['annotated_005.json', 'annotated_006.json',
+                     'annotated_007.json', 'annotated_008.json',
+                     'annotated_009.json', 'annotated_010.json']
+    for json_file in ANNOTED_FILES:
+        with open(json_file) as jso_file:
+            arr = json.load(jso_file)
+            arr = list(map(clean_name, arr))
+            print(arr)
+            final_result = final_result + arr
+
+    final_result = sorted(final_result, key=lambda k: k['name'])
     with open('best_data.json', 'w+') as ff:
         json.dump(final_result, ff)
